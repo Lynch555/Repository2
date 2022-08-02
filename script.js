39ea2dc93b185480c84beb02b812643ec80596ad
Lynch555 <vishneuski1983@mail.ru>
2022-08-02 12:04:06 +0300

    выполнил lesson2

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = ('myProject');
+let screens = ("Простые, Сложные, Интерактивные");
+let screenPrice = (1000);
+let rollback = (10);
+let fullPrice = (10000);
+let adaptive = (true);
 
-alert("Сообщение с любым текстом");
-console.log("Сообщение с любым текстом в консоль");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+
+console.log(screens.length);
+console.log(screenPrice + " рублей, долларов, гривен, юани");
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback / 100));
