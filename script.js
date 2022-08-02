240c6adfa56eb1314c25ad0e72be80cf3a5a87ba
Lynch555 <vishneuski1983@mail.ru>
2022-08-02 12:28:35 +0300

    ДЗ2 усложненное

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let num = 266219,
+    rezult,
+    rezult2;
 
-alert("Сообщение с любым текстом");
-console.log("Сообщение с любым текстом в консоль");
+rezult = 2 * 6 * 6 * 2 * 1 * 9;
+console.log(rezult);
+
+rezult2 = rezult ** 3;
+console.log(rezult2);
+
+let digit1 = rezult2.toString()[0];
+let digit2 = rezult2.toString()[1];
+console.log(digit1, digit2);
